001121e671f2e7849104acb05ceaf7aba3574ce9
ExcaliburDePaladin <renamenogame@gmail.com>
2024-10-10 11:16:51 +0300

    add global variables to use later in functions

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello')
+
+//global variable to store human's choice
+var human; 
+//global variable to store computer's choice
+var comp;
+//global variable to store human's score
+var humscore;
+//global variable to store computer's score
+var compscore;
